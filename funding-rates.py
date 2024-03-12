5797e7abc184d515837b6f3834b2947781954a1b
Chip Thien <themacrochip@gmail.com>
2024-03-12 11:39:28 -0500

    Filter eagerly

--- funding-rates.py
+++ funding-rates.py
@@ -1,24 +1,19 @@
 import json
 import requests
 
-r = requests.get("https://stats-api.hyperliquid.xyz/hyperliquid/funding_rate?start_date=2023-11-14&end_date=2024-02-25")
+r = requests.get("https://stats-api.hyperliquid.xyz/hyperliquid/funding_rate?start_date=2024-02-27&end_date=2024-03-05")
 data = r.json()["chart_data"]
 
-def list_all_coins(data):
-	return [entry['coin'] for entry in data]
-
-
-def filter_eth_btc(data):
-    return [entry for entry in data if entry['coin'] in ['ETH', 'BTC']]
+def filter_desired_coins(data, coin_filter = ['ETH', 'BTC']):
+    return [entry['coin'] for entry in data if entry['coin'] in coin_filter]
 
 result = {}
 
-for coin in list_all_coins(data):
-    data_points = [item['sum_funding'] for item in data if item['coin'] == coin] #coins might have fewer data points than other coins due to being newer
-    result[coin] = sum(data_points) / len(data_points)
+for coin in filter_desired_coins(data):
+    data_points = [item['sum_funding'] for item in data if item['coin'] == coin]
+    result[coin] = sum(data_points) / len(data_points)  #coins might have fewer data points than other coins due to being newer so divide by len(data_points)
 
 for item in result:
-    if (item == "BTC" or item == "ETH"):
-        print(item)
-        print(round(result[item], 2))
-        print("\n")
+    print(item)
+    print(round(result[item], 2))
+    print("\n")

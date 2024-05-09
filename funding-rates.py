dfa7d7728e9f67e0fb9740b7856089bb7a5f4b22
Chip Thien <themacrochip@gmail.com>
2024-05-09 15:25:28 -0500

    show all coins and sort them by funding rate

--- funding-rates.py
+++ funding-rates.py
@@ -2,7 +2,7 @@ import json
 import requests
 from datetime import datetime
 
-start_date_str = "2024-05-01"
+start_date_str = "2024-04-25"
 end_date_str = "2029-04-16"
 start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
 end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
@@ -13,6 +13,9 @@ print("")
 
 r = requests.get(f"https://d2v1fiwobg9w6.cloudfront.net/funding_rate")
 data = r.json()["chart_data"]
+
+def all_coins(data):
+    return [entry['coin'] for entry in data]
 
 def filter_desired_entries(data, coin_filter):
     result = []
@@ -26,9 +29,9 @@ def all_coins(data):
     return [entry['coin'] for entry in data]
 
 result = {}
-coin_filter = ['ETH', 'BTC', 'SOL', 'RUNE']
+coin_filter = all_coins(data)
 coins = filter_desired_entries(data, coin_filter)
-print(coins)
+# print(coins)
 print(str(len(coins)) + " data points")
 print("")
 
@@ -36,7 +39,9 @@ for coin in coin_filter:
     data_points = [item['sum_funding'] for item in coins if item['coin'] == coin]
     result[coin] = sum(data_points) / len(data_points)  #coins might have fewer data points than other coins due to being newer so divide by len(data_points)
 
-for item in result:
+sorted_result = sorted(result.items(), key=lambda x: x[1], reverse=False)
+
+for item, value in sorted_result:
     print(item)
-    print(round(result[item], 4))
+    print(round(value, 4))
     print("\n")
